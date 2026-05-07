1efa80c640d63f2b6f93a148774cde4b0f6daf16
Alberto Madrid <alberto.madrid@udla.edu.ec>
2026-05-07 15:20:11 -0500

    cuarto commit

--- ejercicioheaders2.c
+++ ejercicioheaders2.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "sumar.h"
+#include "restar.h"
+#include "multiplicar.h"
+#include "division.h"
 int main (int argc, char *argv[]) 
 
 {   int a, b;
@@ -8,6 +11,9 @@ int main (int argc, char *argv[])
     printf("Ingrese 2er numero: ");
     scanf("%d", &b);
     printf ("%d ",sumar(a,b));
+    printf ("%d",restar(a,b));
+    printf ("%d",multiplicar(a,b));
+    printf ("%d",dividir(a,b));
     return 0;
     
 }   
